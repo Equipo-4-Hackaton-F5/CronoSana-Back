24840fde61f06f043a4fe1aebcbd36ea683303ce
Yolanda <yolandaalfonsolopez@gmail.com>
2025-09-18 00:51:39 +0200

    feat(IService, InterfaceMedicineService): service created

---
+++ src/main/java/com/equipo4hackathonf5/CronoSana/implementations/IService.java
@@ -0,0 +1,11 @@
+package com.equipo4hackathonf5.CronoSana.implementations;
+
+import java.util.List;
+
+public interface IService <T, S> {
+    public List<T> getEntities();
+    public T createEntity(S dto);
+    //public T getById(Long id);
+    public T updateEntity(Long id, S dto);
+    public void deleteEntity(Long id);
+}
