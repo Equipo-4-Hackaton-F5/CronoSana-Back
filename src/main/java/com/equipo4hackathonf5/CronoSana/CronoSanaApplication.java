9003448ddd5087d5ff84754058a187e7d19af7aa
Yelyzaveta Krasiuk <lizakrasyuk24@gmail.com>
2025-09-17 11:55:42 +0200

    init: init repo

---
+++ src/main/java/com/equipo4hackathonf5/CronoSana/CronoSanaApplication.java
@@ -0,0 +1,13 @@
+package com.equipo4hackathonf5.CronoSana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CronoSanaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CronoSanaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/equipo4hackathonf5/CronoSana/CronoSanaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.equipo4hackathonf5.CronoSana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CronoSanaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
