9a8e908a02023d2e07d73c60d03848a36b439945
More-Pe <peraltamorena.va@gmail.com>
2025-09-18 00:29:16 +0200

    feat(ScheduleRepository): add repository

---
+++ src/main/java/com/equipo4hackathonf5/CronoSana/schedule/ScheduleRepository.java
@@ -0,0 +1,6 @@
+package com.equipo4hackathonf5.CronoSana.schedule;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ScheduleRepository extends JpaRepository<ScheduleEntity, Long> {
+}
